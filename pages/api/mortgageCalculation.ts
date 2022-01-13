31dd64ddcc4bb453fb106bb263f12604a1ba0926
Liam Levesque <liam.levesque@produce8.com>
2022-01-13 12:52:06 -0800

    hello world

---
+++ pages/api/mortgageCalculation.ts
@@ -0,0 +1,41 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  monthlyPayment?: string;
+  error?: string;
+}
+
+type MortgageParams = {
+  principal: number;
+  annualInterestRate: number;
+  termOfLoan: number; 
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if(req.method === 'POST'){
+    // const value = req.value;
+    const {principal, annualInterestRate, termOfLoan} = req.query;
+    const values = calculatePayment({principal: parseInt(principal.toString()), annualInterestRate:parseInt(annualInterestRate.toString()), termOfLoan:parseInt(termOfLoan.toString())});
+    console.log('values', values);
+    res.status(200).json({ monthlyPayment: values })
+}else{
+    res.status(400).json({ error: 'Only accepts POST' })
+}
+}
+
+
+
+function calculatePayment({principal, annualInterestRate, termOfLoan}:MortgageParams) {
+
+    var percentageRate = annualInterestRate / 1200;
+    var lengthOfLoan = 12 * termOfLoan;
+    var monthlyPayment = (principal * percentageRate) / (1 - (Math.pow((1 + percentageRate) , lengthOfLoan * -1)));
+    const payment = monthlyPayment.toFixed(2);
+
+
+    return payment;
+} 
