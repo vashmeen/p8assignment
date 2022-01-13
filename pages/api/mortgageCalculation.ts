b9bdb87c9aae15ff4dd70ed0aeabb5ae8692a1ed
Liam Levesque <liam.levesque@produce8.com>
2022-01-13 13:04:23 -0800

    cleanup

--- pages/api/mortgageCalculation.ts
+++ pages/api/mortgageCalculation.ts
@@ -16,25 +16,21 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  if(req.method === 'POST'){
-    const {principal, annualInterestRate, termOfLoan} = req.query;
-    const values = calculatePayment({principal: parseInt(principal.toString()), annualInterestRate:parseInt(annualInterestRate.toString()), termOfLoan:parseInt(termOfLoan.toString())});
-    if(values !== "NaN") res.status(200).json({ monthlyPayment: values })
-    else res.status(400).json({ error: 'There was a problem calculating your mortgage. Please check your inputs' })
-}else{
-    res.status(400).json({ error: 'Only accepts POST' })
+    if(req.method === 'POST'){
+      const {principal, annualInterestRate, termOfLoan} = req.query;
+      const values = calculatePayment({principal: parseInt(principal.toString()), annualInterestRate:parseInt(annualInterestRate.toString()), termOfLoan:parseInt(termOfLoan.toString())});
+      if(values !== "NaN") res.status(200).json({ monthlyPayment: values })
+      else res.status(400).json({ error: 'There was a problem calculating your mortgage. Please check your inputs' })
+  }else{
+      res.status(400).json({ error: 'Only accepts POST' })
+  }
 }
-}
-
-
 
 function calculatePayment({principal, annualInterestRate, termOfLoan}:MortgageParams) {
-
     var percentageRate = annualInterestRate / 1200;
     var lengthOfLoan = 12 * termOfLoan;
     var monthlyPayment = (principal * percentageRate) / (1 - (Math.pow((1 + percentageRate) , lengthOfLoan * -1)));
     const payment = monthlyPayment.toFixed(2);
-
 
     return payment;
 } 
