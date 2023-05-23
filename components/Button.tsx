9ad5d0e8450b835834fdf93114ab453ca949b1ab
vashmeen <vashmeen@eva.mozmail.com>
2023-05-22 17:37:42 -0700

    feat: improve a11y

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,7 +6,7 @@ const Button = ({ children, className, ...props }: Props) => {
   return (
     <button
       type='button'
-      className={`text-white bg-purple-900 hover:bg-purple-800 focus:outline-none focus:ring-4 focus-visible:ring-purple-300 font-bold rounded-full text-xl px-6 py-3 text-center  ${className}`}
+      className={`text-white bg-purple-900 hover:bg-purple-800  font-bold rounded-full text-xl px-6 py-3 text-center  ${className}`}
       {...props}
     >
       {children}
