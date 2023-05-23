183932986876d00ba9618ab1b0db97188b8bc312
vashmeen <vashmeen@eva.mozmail.com>
2023-05-22 19:25:21 -0700

    feat: make header sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ import logo from '../public/p8Wordmark_sm.svg';
 import Image from 'next/image';
 const Header = () => {
   return (
-    <header className='bg-white shadow-2xl shadow-blue-200 px-4'>
+    <header className='bg-white shadow-2xl shadow-blue-200 px-4 sticky top-0'>
       <div className='flex gap justify-between  mx-auto max-w-screen-lg py-3 '>
         <Link href='/'>
           <a className='flex items-center'>
